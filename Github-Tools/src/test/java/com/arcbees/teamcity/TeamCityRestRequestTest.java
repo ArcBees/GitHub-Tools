d39b3b048a0abd287cf6a5d496a3cd43a650b825
Brandon Donnelson <branflake2267@gmail.com>
2013-02-09 13:28:08 -0800

    change settings paths to relative

--- Github-Tools/src/test/java/com/arcbees/teamcity/TeamCityRestRequestTest.java
+++ Github-Tools/src/test/java/com/arcbees/teamcity/TeamCityRestRequestTest.java
@@ -18,7 +18,7 @@ public class TeamCityRestRequestTest {
 
     @Before
     public void setup() throws SAXException, IOException, ParserConfigurationException {
-        MavenProperties properties = new MavenProperties("/Users/branflake2267/.m2/settings.xml");
+        MavenProperties properties = new MavenProperties("~/.m2/settings.xml");
         properties.fetchProperties();
         
         github = properties.getGithubCredentials("github");
