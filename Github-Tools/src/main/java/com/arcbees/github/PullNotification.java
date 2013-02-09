73397ecffbe20fd5ea70ed90e2b8ef70ab78f963
Brandon Donnelson <branflake2267@gmail.com>
2013-02-09 00:44:38 -0800

    adding in param parser

--- Github-Tools/src/main/java/com/arcbees/github/PullNotification.java
+++ Github-Tools/src/main/java/com/arcbees/github/PullNotification.java
@@ -29,23 +29,106 @@ public class PullNotification {
 
     private GitHubClient client;
 
-    // TODO params
+    /**
+     * Parameter -ro=branflake2267
+     * Repository Owner [user or organization]
+     */
     private String repoOwner = "branflake2267";
+
+    /**
+     * Parameter -rn=Sandbox
+     * Repository Name - like [organization/name] or [user/name] - only include the name
+     */
     private String repoName = "Sandbox";
+
+    /**
+     * Parameter -sha=c9dc29c60df9bf8af8248e77db0e2eca1d7b7e4d
+     * Ref Sha which is Team City parameter %build.vcs.number%
+     */
     private String commitShaRef = "c9dc29c60df9bf8af8248e77db0e2eca1d7b7e4d";
 
+    /**
+     * Parameter -settings=~/.m2/settings.xml 
+     * Override the default maven settings url.
+     */
     private String mavenSettingsPath = "~/.m2/settings.xml";
+
+    /**
+     * Parameter -github=github
+     * Store the github credentials as a server. <server> <id>github</id> <username>branflake2267</username>
+     * <password>xxxxxxx</password> </server>
+     */
     private String mavenSettingsGithubServerId = "github";
+
+    /**
+     * Parameter -teamcity=teamcity-gonevertical
+     * Store the Team City credentials as a server. <server> <id>teamcity-gonevertical</id>
+     * <username>branflake2267</username> <password>xxxxxxx</password> <url>http://teamcity.gonevertical.org</url>
+     * </server>
+     */
     private String mavenSettingsTeamcityServerId = "teamcity-gonevertical";
+
+    /**
+     * Parameter -returnurl=http://teamcity.gonevertical.org
+     * Provide a return link for build server investigation
+     */
     private String buildServerReturnUrl = "http://teamcity.gonevertical.org";
+
+    /**
+     * Parameter -status=pending 
+     * Provide a manual status update
+     */
     private String status = "error";
+    
+    /**
+     * Parameter -buildid=299
+     * Provide the buildId
+     */
+    private int buildId = 299;
 
     private MavenProperties properties;
 
     public PullNotification(String[] args) {
-        // TODO args
+        parameterParser(args);
 
-        // TODO if status is not null, change to it.
+        // TODO validate params and output missing
+    }
+
+    private void parameterParser(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-ro")) {
+                repoOwner = parameterParser(args[i]);
+            } else if (args[i].equals("-rn")) {
+                repoName = parameterParser(args[i]);
+            } else if (args[i].equals("-sha")) {
+                commitShaRef = parameterParser(args[i]);
+            } else if (args[i].equals("-settings")) {
+                String path = parameterParser(args[i]);
+                if (path != null) {
+                    mavenSettingsPath = path;
+                }
+            } else if (args[i].equals("-github")) {
+                mavenSettingsGithubServerId = parameterParser(args[i]);
+            } else if (args[i].equals("-teamcity")) {
+                mavenSettingsTeamcityServerId = parameterParser(args[i]);
+            } else if (args[i].equals("-returnurl")) {
+                buildServerReturnUrl = parameterParser(args[i]);
+            } else if (args[i].equals("-status")) {
+                status = parameterParser(args[i]);
+            } else if (args[i].equals("-buildid")) {
+                String id = parameterParser(args[i]);
+                buildId = Integer.valueOf(id);
+            }
+        }
+    }
+
+    private String parameterParser(String param) {
+        if (param == null || param.trim().length() == 0 || param.contains("=") == false) {
+            return null;
+        }
+
+        String[] values = param.split("=");
+        return values[1];
     }
 
     private void run() {
@@ -64,7 +147,7 @@ public class PullNotification {
         }
 
         try {
-            autoChangeStatus(299);
+            autoChangeStatus(buildId);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -114,7 +197,7 @@ public class PullNotification {
         status.setTargetUrl(buildServerReturnUrl);
         status.setCreatedAt(new Date());
         status.setUpdatedAt(new Date());
-        
+
         changeStatus(status);
     }
 
@@ -127,8 +210,7 @@ public class PullNotification {
 
     private Repository getRepository() throws IOException {
         RepositoryService repoService = new RepositoryService(client);
-        Repository  repo = repoService.getRepository(repoOwner, repoName);
-        return repo;
+        return repoService.getRepository(repoOwner, repoName);
     }
 
     /**
